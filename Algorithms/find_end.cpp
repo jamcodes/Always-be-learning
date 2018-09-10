df18e966b3d76b77579db95743d84d839b1e5d16
Jeremi Mucha <jeremimucha@gmail.com>
2018-09-10 19:21:31 +0200

    Add std::find_end algorithm example

---
+++ Algorithms/find_end.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <array>
+#include <vector>
+#include <iterator>
+#include "auxiliary.h"
+
+
+
+int main()
+{
+    std::vector<int> coll;
+    std::array<int, 4> subcoll{3,4,5,6};
+    const auto gen = [i=0]()mutable noexcept{return i++;};
+    std::generate_n(std::back_inserter(coll), 7, gen);
+    std::generate_n(std::back_inserter(coll), 7, gen);
+    print_elements(coll, "coll: ");
+    print_elements(subcoll, "subcoll: ");
+
+    const auto it1 = std::find_end(coll.cbegin(), coll.cend(),          // range
+                                   subcoll.cbegin(), subcoll.cend());   // subrange
+    if (it1 != coll.cend()) {
+        std::cout << "last subcoll found starting at index "
+            << std::distance(coll.cbegin(), it1) << "\n";
+    } else {
+        std::cout << "subcoll not found\n";
+    }
+
+    const auto it2 = std::find_end(coll.cbegin(), coll.cend(),          // range
+                                   subcoll.cbegin(), subcoll.cend(),    // subrange
+                                   [](int el, int subel)noexcept{       // binary predicate
+                                       return el == --subel;}
+                                    );
+    if (it2 != coll.cend()) {
+        std::cout << "last subcoll meeting the binary predicate requirements\nstarts at index: "
+            << std::distance(coll.cbegin(), it2) << "\n";
+    } else {
+        std::cout << "subcoll meeting the binary predicate not found\n";
+    }
+}
