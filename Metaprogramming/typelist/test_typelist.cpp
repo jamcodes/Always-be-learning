ecf74df6579c2ff65d94edfb388f98e245a038b4
Jeremi Mucha <jam.sfinae@gmail.com>
2019-02-17 06:55:48 +0100

    Add a basic typelist + algorithms implementation

---
+++ Metaprogramming/typelist/test_typelist.cpp
@@ -0,0 +1,27 @@
+#include "typelist.hpp"
+#include "accumulate.hpp"
+#include "commontype.hpp"
+#include "findindexof.hpp"
+#include "identity.hpp"
+#include "ifthenelse.hpp"
+#include "insertion_sort.hpp"
+#include "is_empty.hpp"
+#include "largest_type.hpp"
+#include "nth_element.hpp"
+#include "pop_back.hpp"
+#include "replace.hpp"
+#include "reverse.hpp"
+#include "transform.hpp"
+#include "typelist.hpp"
+#include "value_lists.hpp"
+#include "erase.hpp"
+#include "remove_duplicates.hpp"
+
+#include "is_derived.hpp"
+#include "most_derived.hpp"
+#include "most_derived_first.hpp"
+
+int main()
+{
+
+}
